7fd0725059371bc39d711961a63a6cd114f0184a
Kevintirta <kevintirta1@yahoo.com>
2020-04-19 17:52:39 +0800

    first commit

---
+++ MovieApp/GenreButtonScreen.swift
@@ -0,0 +1,30 @@
+//
+//  GenreButtonScreen.swift
+//  MovieApp
+//
+//  Created by Kevin tirta Gondokusumo on 19/4/20.
+//  Copyright © 2020 Kevin tirta Gondokusumo. All rights reserved.
+//
+
+import UIKit
+
+class GenreButtonScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
